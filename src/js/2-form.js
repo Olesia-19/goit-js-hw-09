e7fc81a1736ffa3c76b4dfbd475838b87517ee80
olesia <ldsolesya@gmail.com>
2025-05-05 20:19:26 +0100

    hw 9

---
+++ src/js/2-form.js
@@ -0,0 +1,67 @@
+const formData = {
+    email: '',
+    message: '',
+}
+
+const STORAGE_KEY = 'data'
+
+const form = document.querySelector('.feedback-form')
+
+populateData();
+
+form.addEventListener('submit', handleSubmitForm);
+form.addEventListener('focusout', handleFormInput);
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  form.reset();
+}
+
+function handleFormInput(e) {
+  const { name, value } = e.target;
+  formData[name] = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateData() {
+  const savedLSData = localStorage.getItem(STORAGE_KEY);
+  if (!savedLSData) return;
+
+  try {
+    const dataFromLS = JSON.parse(savedLSData);
+    
+    for (const [key, value] of Object.entries(dataFromLS)) {
+      if (form.elements[key]) {
+        form.elements[key].value = value;
+        formData[key] = value; 
+      }
+    }
+  }
+
+  catch (error) {
+    alert("Something went wrong");
+  }
+}
+
+
+
+
+const input = document.querySelector('input[name="email"]');
+
+input.addEventListener('focus', () => {
+  input.placeholder = 'Type area';
+});
+
+input.addEventListener('blur', () => {
+  input.placeholder = '';
+});
+
